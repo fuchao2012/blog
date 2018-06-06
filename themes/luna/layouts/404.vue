247eb98419035ee8fa4d28680f381b00db256b05
EGOIST <0x142857@gmail.com>
2018-06-06 11:07:12 +0800

    add 404 page

---
+++ themes/luna/layouts/404.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="notfound">
+    <h1>Page Not Found :(</h1>
+    <span v-if="isDemo">maybe it's moved to <a :href="demoURL" target="_blank">{{ demoURL }}</a></span>
+    <div class="backhome">
+      <router-link to="/">← Back to HomePage</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+const DEMO_RE = /^\/([\w\-]+)$/
+export default {
+  computed: {
+    isDemo() {
+      return DEMO_RE.test(this.$route.path)
+    },
+    demoURL() {
+      return this.isDemo && `https://${DEMO_RE.exec(this.$route.path)[1]}.egoist.rocks`
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  font-family: Helvetica;
+  font-weight: 300;
+  font-size: 3rem;
+}
+
+.notfound {
+  max-width: 500px;
+  margin: 0 40px;
+}
+
+.backhome {
+  margin: 20px 0;
+}
+</style>
